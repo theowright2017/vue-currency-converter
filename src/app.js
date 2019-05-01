4a3db1575547dc8f8aecf61201b725790488e07f
Theo Wright <theowright2015@gmail.com>
2019-05-01 10:28:07 +0100

    final commit, corrected

--- src/app.js
+++ src/app.js
@@ -3,12 +3,29 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#whole-container',
+
+
+    //
+    //
+    //
+    // mounted() {
+    //   fetch('https://api.exchangeratesapi.io/latest')
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))
+    //   }
+    //
+      //fetch we dont know how long it will take
+      //modern javascript function es6 written in this way
+      //usually put into method to reuse
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     data: {
       currencies: {},
-      rates: {},
       conversionAmount: 0,
-      currencyToConvertTo: "",
-      newAmount: 0
+      baseCurrency: "",
+      result: null
+      //set result to null as has falsey value, so we can do v-if to only display when has value which changes it to truthy
     },
 
 
@@ -17,23 +34,33 @@ document.addEventListener('DOMContentLoaded', () => {
         fetch('https://api.exchangeratesapi.io/latest')
         .then(response => response.json())
         .then((data) => {
-          this.currencies = data;
+          this.currencies = data.rates;
         });
       },
 
-      convertTo: function (amount) {
-        event.preventDefault();
-      // i cant write the correct function as i dont know how to access the value of a key that constantly changes.....................
-
+      convertTo: function () {
+        event.preventDefault()
+        this.result = this.conversionAmount * this.currencies[this.baseCurrency]
+        //use [] to access object that has changing variables, because this is using the variable result i.e NZD or GBP and inputting it to ths.currencies
       }
-
     },
-
+      filters: {
+        twoDecimalPlaces: function(number) {
+          return number.toFixed(2);
+        }
+        //filters are essentially functions to be reused throughout, for things like decimal place etc.  in return in html written as
+        // {{ result | filterName }}
+    },
     // computed: {
     //
     // },
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     mounted() {
       this.fetchInfo();
     }
+
+
+
   })
 });
